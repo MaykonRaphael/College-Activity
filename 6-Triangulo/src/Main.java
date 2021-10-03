26543a82492ec09d372d6ff09df91b3308dbf114
Maykon Raphael <mraphaeld.batista@gmail.com>
2021-10-03 16:31:29 -0300

    feat: Exercicio 6

---
+++ 6-Triangulo/src/Main.java
@@ -0,0 +1,41 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		Ler 3 valores (A, B e C) representando as medidas dos lados de um triângulo e
+//		escrever se formam ou não um triângulo. OBS: para formar um triângulo, o valor de
+//		cada lado deve ser menor que a soma dos outros 2 lados.
+
+		double A, B, C;
+		
+		A = setA();
+		B = setB();
+		C = setC();
+		
+		verifyTriangle(A,B,C);
+
+	}
+
+	private static void verifyTriangle(double a, double b, double c) {
+		if( a + b > c && a + c > b && b + c > a ) {
+			JOptionPane.showMessageDialog(null, "Os valores formam um triangulo!");
+		} else {
+			JOptionPane.showMessageDialog(null, "Os valores nao formam um triangulo!");
+		}
+		
+	}
+
+	private static double setA() {
+		return Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do lado A!"));
+	}
+
+	private static double setB() {
+		return Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do lado B!"));
+	}
+
+	private static double setC() {
+		return Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do lado C!"));
+	}
+
+}
