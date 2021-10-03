a324bd91691ff584c06e16839eb1d0cb6c679766
Maykon Raphael <mraphaeld.batista@gmail.com>
2021-10-03 15:51:08 -0300

    feat: Exercicio 5

--- 5-HorasTrabalho/src/Main.java
+++ 5-HorasTrabalho/src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.JOptionPane;
 
 public class Main {
 
@@ -8,8 +9,35 @@ public class Main {
 //		trabalhadas em um mês, o salário por hora e escreva o salário total do funcionário,
 //		que deverá ser acrescido das horas extras, caso tenham sido trabalhadas (considere
 //		que o mês possua 4 semanas exatas).
+		
+		double timeMonthWork, hourWage;
+		
+		timeMonthWork = setTimeMonthWork();
+		hourWage = setHourWage();
+		
+		calculateExtraHour(timeMonthWork,hourWage);
 
+	}
 
+	private static void calculateExtraHour(double timeWeekWork, double hourWage) {
+		double extraWeekHour = (timeWeekWork/4 - 40), hourExtraWage = 0;
+		if( timeWeekWork / 4 > 40 ) {
+			timeWeekWork = (timeWeekWork/4 - extraWeekHour);
+			hourExtraWage = hourWage + hourWage * 0.5;
+			JOptionPane.showMessageDialog(null, "Seu salario com hora extra: R$" + 
+											((timeWeekWork * hourWage) + (extraWeekHour * hourExtraWage)) * 4);
+		} else {
+			JOptionPane.showMessageDialog(null, "Seu salario: R$" + (timeWeekWork * hourWage));
+		}
+		
+	}
+
+	private static double setHourWage() {
+		return Double.parseDouble(JOptionPane.showInputDialog("Digite quanto voce ganha por hora trabalhada!"));
+	}
+
+	private static int setTimeMonthWork() {
+		return Integer.parseInt(JOptionPane.showInputDialog("Digite quantas horas foram trabalhadas no mes!"));
 	}
 
 }
